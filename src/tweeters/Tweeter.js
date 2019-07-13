98a141231995707051db5be2561b2bf35a54c81b
Jaid <jaid.jsx@gmail.com>
2019-07-13 05:03:40 +0200

    improve: Added more logging
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/tweeters/Tweeter.js
+++ src/tweeters/Tweeter.js
@@ -2,6 +2,8 @@ import config from "lib/config"
 import got from "got"
 import logger from "lib/logger"
 
+let currentIndex = 1
+
 export default class Tweeter {
 
   static apiGot = got.extend({
@@ -12,10 +14,13 @@ export default class Tweeter {
 
   constructor(handle) {
     this.handle = handle
-    logger.info("Registered tweeter for @%s", handle)
+    this.index = currentIndex
+    currentIndex++
+    logger.info("Registered tweeter #%s for @%s", this.index, handle)
   }
 
   async post(text) {
+    logger.info("[Tweeter #%s] @%s: %s", this.index, this.handle, text)
     try {
       await Tweeter.apiGot.post("tweet", {
         body: {
@@ -26,7 +31,7 @@ export default class Tweeter {
         } |> JSON.stringify,
       })
     } catch (error) {
-      logger.error("Could not send \"%s\" as %s: %s", text, this.handle, error)
+      logger.error("[Tweeter #%s] Could not send tweet: %s", this.index, error)
     }
   }
 
