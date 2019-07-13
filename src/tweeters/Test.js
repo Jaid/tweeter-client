f085ca2eb15e5d13bac0bdd178bf7a9bbeb16667
Jaid <jaid.jsx@gmail.com>
2019-07-13 14:10:47 +0200

    Removed `canvas` again
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/tweeters/Test.js
+++ src/tweeters/Test.js
@@ -1,4 +1,3 @@
-import {createCanvas} from "canvas"
 import fsp from "@absolunet/fsp"
 
 import Tweeter from "./Tweeter"
@@ -15,16 +14,7 @@ export default class extends Tweeter {
   }
 
   async run() {
-    const canvas = createCanvas(1920, 1080)
-    const context = canvas.getContext("2d")
-    context.font = "200px \"Comic Sans\""
-    context.fillStyle = "#00CC00"
-    context.fillText("Everyone hates this font :(", 10, 500)
-    const buffer = canvas.toBuffer("image/png", {
-      compressionLevel: 9,
-    })
-    await fsp.outputFile("./dist/image.png", buffer)
-    // this.post(`${_PKG_TITLE} v${_PKG_VERSION}, ${Date.now()}`)
+    this.post(`${_PKG_TITLE} v${_PKG_VERSION}, ${Date.now()}`)
   }
 
 }
