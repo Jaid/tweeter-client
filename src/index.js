4854c2bd9b195f231e35891474a6757dcf67fa7d
GitHub Action <action@github.com>
2020-03-27 01:05:56 +0000

    autofix: src/index.js should have md5 e4263b9fef877e02eda6188d0e51b9c8

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+import yargs from "yargs"
+
 import core from "./core"
 
-const job = async () => {
+function logError(message) {
+  if (core?.logger?.error) {
+    core.logger.error(message)
+  } else {
+    console.error(message)
+  }
+}
+
+process.on("unhandledRejection", error => {
+  if (error) {
+    logError(`Unhandled promise rejection: ${error?.message || error}`)
+  } else {
+    logError("Unhandled promise rejection")
+  }
+  if (error?.stack) {
+    logError(error.stack)
+  }
+})
+
+async function job() {
   const plugins = {}
-  const pluginsRequire = require.context("./plugins/", true, /index.js$/)
+  const pluginsRequire = require.context("./plugins/", true, /^\.\/\w+\/index.js$/)
   for (const value of pluginsRequire.keys()) {
     const {pluginName} = value.match(/[/\\](?<pluginName>.+?)[/\\]index\.js$/).groups
     plugins[pluginName] = pluginsRequire(value).default
@@ -10,4 +31,15 @@ const job = async () => {
   await core.init(plugins)
 }
 
-job()
+function main() {
+  job().catch(error => {
+    logError("Core process crashed")
+    logError(error)
+    process.exit(1)
+  })
+}
+
+yargs
+  .scriptName(_PKG_NAME)
+  .version(_PKG_VERSION)
+  .command("$0", _PKG_DESCRIPTION, {}, main).argv
