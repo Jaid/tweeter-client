d97cfd6d6e85c66a0e48c6df5417885c4e343e1d
Jaid <jaid.jsx@gmail.com>
2020-03-27 09:20:21 +0100

    feat: Finished DeadByDaylightBuild tweeter
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/tweeters/DeadByDaylightBuild.js
+++ src/tweeters/DeadByDaylightBuild.js
@@ -1,9 +1,14 @@
+import fsp from "@absolunet/fsp"
 import joi from "@hapi/joi"
 import deadByDaylight from "dead-by-daylight"
+import handlebars from "handlebars"
+import humanizeList from "humanize-list"
+import Jimp from "jimp"
 import schedule from "node-schedule"
 import pickRandom from "pick-random"
 import renderDeadByDaylightBuild from "render-dead-by-daylight-build"
 
+import collator from "lib/collator"
 import Tweeter from "lib/Tweeter"
 
 /**
@@ -11,20 +16,78 @@ import Tweeter from "lib/Tweeter"
  * @prop {string} cron
  */
 
+function sortPerks(perk1, perk2) {
+  return collator.compare(perk1.title, perk2.title)
+}
+
 export default class extends Tweeter {
 
   static displayName = "DeadByDaylightBuild"
 
   static schema = joi.object().keys({
-    type: joi.string(),
+    perksType: joi.string(),
     cron: joi.string(),
     backgroundFile: joi.string().required(),
+    template: joi.string().default("Today's Dead by Daylight {{{options.perksType}}} build contains {{{perks}}}."),
   })
 
-  start() {
+  async start() {
+    this.template = handlebars.compile(this.options.template)
+    if (!this.options.cron) {
+      await this.run()
+    }
     schedule.scheduleJob(this.options.cron, () => {
-      console.log("Triggered")
+      this.run()
     })
+  }
+
+  getPerkPool() {
+    const perks = Object.values(deadByDaylight.perks)
+    if (this.options.perksType) {
+      return perks.filter(perk => {
+        if (!perk.visible) {
+          return false
+        }
+        return perk.for === this.options.perksType
+      })
+    }
+    return perks.filter(perk => {
+      if (!perk.visible) {
+        return false
+      }
+      return true
+    })
+  }
+
+  getPicks() {
+    const perks = this.getPerkPool()
+    const picks = pickRandom(perks, {count: 4})
+    picks.sort(sortPerks)
+    return picks
+  }
+
+  getText(picks) {
+    return this.template({
+      perks: humanizeList(picks.map(perk => perk.title)),
+      options: this.options,
+    })
+  }
+
+  async getBackgroundBuffer() {
+    const backgroundBuffer = await fsp.readFile(this.options.backgroundFile)
+    return backgroundBuffer
+  }
+
+  async run() {
+    const picks = this.getPicks()
+    const pickIds = picks.map(perk => perk.id)
+    const foregroundBuffer = await renderDeadByDaylightBuild(pickIds)
+    const foregroundJimp = await Jimp.create(foregroundBuffer)
+    const backgroundBuffer = await this.getBackgroundBuffer()
+    const backgroundJimp = await Jimp.create(backgroundBuffer)
+    backgroundJimp.composite(foregroundJimp, 0, 0)
+    const outputBuffer = await backgroundJimp.getBase64Async(Jimp.MIME_PNG)
+    await this.post(this.getText(picks), outputBuffer)
   }
 
 }
