02779539aa83ebe9eb671c380820b3cd36b51287
Jaid <jaid.jsx@gmail.com>
2020-04-06 05:48:18 +0200

    Improved test file structure
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -8,8 +8,10 @@ import getQrCodeFromBuffer from "lib/getQrCodeFromBuffer"
 import isLikelyDesign from "lib/isLikelyDesign"
 import renderDeadByDaylightBuild from "lib/renderDeadByDaylightBuild"
 
+const imagesDirectory = path.join(__dirname, "images")
+
 it("should parse an Animal Crossing code", async () => {
-  const imageFile = path.join(__dirname, "AnimalCrossingDesign.png")
+  const imageFile = path.join(imagesDirectory, "AnimalCrossingDesign.png")
   const buffer = await fsp.readFile(imageFile)
   const qrCode = await getQrCodeFromBuffer(buffer)
   const byteArray = new Uint8Array(qrCode.binaryData)
@@ -24,7 +26,7 @@ it("should parse an Animal Crossing code", async () => {
 })
 
 it("should parse an Animal Crossing tune", async () => {
-  const imageFile = path.join(__dirname, "AnimalCrossingTune.jpg")
+  const imageFile = path.join(imagesDirectory, "AnimalCrossingTune.jpg")
   const buffer = await fsp.readFile(imageFile)
   const qrCode = await getQrCodeFromBuffer(buffer)
   const byteArray = new Uint8Array(qrCode.binaryData)
@@ -34,7 +36,7 @@ it("should parse an Animal Crossing tune", async () => {
 })
 
 it("should flag an unrelated QR code as negative", async () => {
-  const imageFile = path.join(__dirname, "AnimalCrossingUnrelated.jpg")
+  const imageFile = path.join(imagesDirectory, "AnimalCrossingUnrelated.jpg")
   const buffer = await fsp.readFile(imageFile)
   const qrCode = await getQrCodeFromBuffer(buffer)
   const byteArray = new Uint8Array(qrCode.binaryData)
@@ -45,7 +47,7 @@ it("should flag an unrelated QR code as negative", async () => {
 })
 
 it("should parse a Pokemon GO friend code", async () => {
-  const imageFile = path.join(__dirname, "PokemonGoFriendCode.jpg")
+  const imageFile = path.join(imagesDirectory, "PokemonGoFriendCode.jpg")
   const buffer = await fsp.readFile(imageFile)
   const qrCode = await getQrCodeFromBuffer(buffer)
   expect(qrCode.binaryData.length).toBe(12)
@@ -54,7 +56,7 @@ it("should parse a Pokemon GO friend code", async () => {
 
 it("should parse a Pokemon GO friend code 2", async () => {
   // Taken from https://twitter.com/Kaikonyan/status/1244538807422197762
-  const imageFile = path.join(__dirname, "PokemonGoFriendCodeScreenshot.jpg")
+  const imageFile = path.join(imagesDirectory, "PokemonGoFriendCodeScreenshot.jpg")
   const buffer = await fsp.readFile(imageFile)
   const qrCode = await getQrCodeFromBuffer(buffer)
   expect(qrCode.binaryData.length).toBe(12)
@@ -69,7 +71,7 @@ it("should render Dead by Daylight perk build", async () => {
     "aceInTheHole",
     "sprintBurst",
   ]
-  const backgroundFile = path.join(__dirname, "deadByDaylightBackground.png")
+  const backgroundFile = path.join(imagesDirectory, "deadByDaylightBackground.png")
   const backgroundBuffer = await fsp.readFile(backgroundFile)
   const buffer = await renderDeadByDaylightBuild(perkIds, backgroundBuffer)
   const file = path.resolve(__dirname, "..", "dist", "test", "deadByDaylightBuild.png")
